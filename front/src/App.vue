7214edee3ae5cf8b0092d5bd501d0aa2eb3d1899
soraincloud <soraincloud@gmail.com>
2024-06-11 01:05:38 +0800

    update：完成个人信息悬浮窗位置相关逻辑

--- front/src/App.vue
+++ front/src/App.vue
@@ -3,7 +3,7 @@
     <el-container>
       <el-header>
         <el-menu
-        :default-active="deafultActiveMenu"
+        :default-active="defaultActiveMenu"
         :ellipsis="false"
         mode="horizontal"
         router
@@ -36,7 +36,45 @@
       </el-main>
     </el-container>
     <el-card class="app-user-message-card" :style="[ userMessageStyleLeft,userMessageStyleTop ]">
-      123
+      <div>
+        <el-button class="app-user-message-card-switch-button" v-if="!isHide" @click="clickHide" link>
+          <el-icon>
+            <ArrowDownBold />
+          </el-icon>
+        </el-button>
+        <el-button class="app-user-message-card-switch-button" v-if="isHide" @click="clickShow" link>
+          <el-icon>
+            <ArrowUpBold />
+          </el-icon>
+        </el-button>
+      </div>
+      <div class="app-user-message-card-div">
+        <div v-if="isSign" class="app-user-message-card-avatar-div">
+          <el-avatar :src="avatarUrl"></el-avatar>
+          <span class="app-user-message-card-avatar-span">{{ username }}</span>
+        </div>
+        <div v-if="!isSign">
+          <el-button type="primary" class="app-user-message-card-sign-button" text>{{ $t("common.signin") }}</el-button>
+          <el-button type="primary" class="app-user-message-card-sign-button" text>{{ $t("common.signup") }}</el-button>
+        </div>
+        <div>
+          <el-button class="app-user-message-card-setting-button" text circle >
+            <el-icon>
+              <Collection />
+            </el-icon>
+          </el-button>
+          <el-button class="app-user-message-card-setting-button" text circle >
+            <el-icon>
+              <Operation />
+            </el-icon>
+          </el-button>
+          <el-button class="app-user-message-card-setting-button" text circle >
+            <el-icon>
+              <SetUp />
+            </el-icon>
+          </el-button>
+        </div>
+      </div>
     </el-card>
   </div>
 </template>
@@ -45,20 +83,24 @@
 import { useDark, useToggle } from '@vueuse/core'
 import { useRoute } from 'vue-router'
 import { ref,computed } from 'vue'
+import axios from 'axios';
 
 const isDark = useDark()//黑暗模式所需变量
 let route = useRoute()
-const deafultActiveMenu = computed( () => { return route.name })//访问页面时默认菜单选项
+const defaultActiveMenu = computed( () => { return route.name })//访问页面时默认菜单选项
 const isDarkModeOpen = ref(false)//当前是否为黑暗模式
 if(localStorage.getItem('vueuse-color-scheme') == 'auto')//通过当前模式设置开关状态
 {
   isDarkModeOpen.value = true
 }
-const userMessageLeft = ref(10)
-const userMessageTop = ref(window.innerHeight - 100)
+const userMessageLeft = ref(10) //悬浮窗距离左侧距离
+const userMessageTop = ref(window.innerHeight - 90) //悬浮窗距离顶部距离
 const userMessageStyleLeft = ref("left: " + userMessageLeft.value + "px;") //个人信息浮动卡片的左侧位置
-const userMessageStyleTop = ref("top: " + userMessageTop.value + "px;") //个人信息浮动卡片的右侧位置
-
+const userMessageStyleTop = ref("top: " + userMessageTop.value + "px;") //个人信息浮动卡片的顶部位置
+const avatarUrl = ref(axios.defaults.baseURL + "/userAvatar/NULL.webp") //头像URL
+const username = ref("null") //用户名显示
+const isSign = ref(false) //是否已经登录
+const isHide = ref(false) //个人信息浮动卡片是否隐藏
 
 const changeDarkMode = () => //改变模式
 {
@@ -71,25 +113,74 @@ const changeDarkMode = () => //改变模式
   )
 }
 
-const loadUserMessagePosition = () => //重新加载个人信息卡片位置
+const clickHide = () => //点击隐藏个人信息卡片
+{
+  isHide.value = true
+  loadUserMessagePosition()
+}
+
+const clickShow = () => //点击显示个人信息卡片
+{
+  isHide.value = false
+  loadUserMessagePosition()
+}
+
+const setUserMessagePosition = () => //重新加载个人信息卡片位置
 {
   userMessageStyleLeft.value = "left: " + userMessageLeft.value + "px;"
   userMessageStyleTop.value = "top: " + userMessageTop.value + "px;"
 }
 
-const windowSizeChange = () => 
+const computeUserMessagePosition = () => //根据是否登录计算个人信息卡片位置
 {
-  userMessageLeft.value = 10
-  userMessageTop.value = window.innerHeight - 100
+  if(isSign.value) //已经登陆
+  {
+    userMessageLeft.value = 10
+    userMessageTop.value = window.innerHeight - 90
+  }
+  else //未登录
+  {
+    userMessageLeft.value = 10
+    userMessageTop.value = window.innerHeight - 80
+  }
+  if(isHide.value)
+  {
+    userMessageTop.value += 50
+  }
+}
+
+const checkSignLocalStorage = () => //检查是否登录的localStorage
+{
+  if(localStorage.getItem("isLogin") ||false) //没有值即为false
+  {
+    isSign.value = true
+  }
+  else
+  {
+    isSign.value = false
+  }
   loadUserMessagePosition()
 }
+
+const loadUserMessagePosition = () => //计算并加载个人信息卡片
+{
+  computeUserMessagePosition()
+  setUserMessagePosition()
+}
+
+const windowSizeChange = () => //窗口大小变化时重新计算并设置个人信息卡片位置
+{
+  loadUserMessagePosition()
+}
+
+checkSignLocalStorage() //初始化时检查一次登录状态
  
 window.addEventListener('resize',windowSizeChange) //监听窗口变动
 
 //下面的内容为解决使用el-table时报错ResizeObserver loop completed with undelivered notifications
-const debounce = (fn, delay) => {
-  let timer
-   return (...args) => {
+const debounce = (fn :any, delay :any) => {
+  let timer :any
+   return (...args :any) => {
      if (timer) {
        clearTimeout(timer)
      }
@@ -101,7 +192,7 @@ const debounce = (fn, delay) => {
   
 const _ResizeObserver = window.ResizeObserver;
 window.ResizeObserver = class ResizeObserver extends _ResizeObserver{
-   constructor(callback) {
+   constructor(callback :any) {
      callback = debounce(callback, 200);
      super(callback);
    }
@@ -153,5 +244,43 @@ window.ResizeObserver = class ResizeObserver extends _ResizeObserver{
 .app-user-message-card
 {
   position: fixed;
+}
+
+.app-user-message-card .el-card__body
+{
+  padding: 10px;
+  padding-top: 5px;
+}
+
+.app-user-message-card-switch-button
+{
+  width: 100%;
+}
+
+.app-user-message-card-div
+{
+  display: flex;
+  align-items: center;
+}
+
+.app-user-message-card-avatar-div
+{
+  display: flex;
+  align-items: center;
+}
+
+.app-user-message-card-avatar-span
+{
+  margin-left: 10px
+}
+
+.app-user-message-card-sign-button
+{
+  margin: 0px !important;
+}
+
+.app-user-message-card-setting-button
+{
+  margin: 0px !important;
 }
 </style>
