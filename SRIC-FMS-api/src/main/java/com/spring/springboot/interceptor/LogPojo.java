73f9de4bd9dfb9cd37eb06c516e034611b3ebcbe
soraincloud <soraincloud@gmail.com>
2024-10-05 19:08:18 +0800

    update：在后端拦截器添加了请求uri前缀的获取 用于权限鉴定

--- SRIC-FMS-api/src/main/java/com/spring/springboot/interceptor/LogPojo.java
+++ SRIC-FMS-api/src/main/java/com/spring/springboot/interceptor/LogPojo.java
@@ -8,9 +8,10 @@ import lombok.Data;
 @Data
 public class LogPojo
 {
-    private String currentTime;
-    private String clientIp;
-    private String requestUrl;
-    private String state;
-    private int stateCode;
+    private String currentTime; //时间
+    private String clientIp; //IP地址
+    private String requestUrl; //请求url
+    private String urlModule; //url前缀权限模块
+    private String state; //登录状态
+    private int stateCode; //登录状态码
 }
