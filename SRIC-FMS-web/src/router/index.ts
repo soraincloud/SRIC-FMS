bff054ab464de0dc08381f51f8934171e2a65ae6
soraincloud <soraincloud@gmail.com>
2024-11-16 21:22:21 +0800

    fix：将Home修改为HomePage

--- SRIC-FMS-web/src/router/index.ts
+++ SRIC-FMS-web/src/router/index.ts
@@ -9,7 +9,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/home',
     name: 'home',
-    component: () => import('@/views/home.vue'),
+    component: () => import('@/views/HomePage.vue'),
   },
   {
     path: '/Hfiles',
